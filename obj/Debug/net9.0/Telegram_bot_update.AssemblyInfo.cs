cabf2ab2473e9505496ef42294e38b1135197313
Valdemar Deutsch <greynervladimir@gmail.com>
2025-01-28 19:48:34 +0400

    Sth not understandable

--- obj/Debug/net9.0/Telegram_bot_update.AssemblyInfo.cs
+++ obj/Debug/net9.0/Telegram_bot_update.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Telegram_bot_update")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2323d83bd3cfe87f34bdf0a92619e9aa9989bd01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7bace3fce84ce9935418eb816067cbdf32089cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Telegram_bot_update")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Telegram_bot_update")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
